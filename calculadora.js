9ec9146492176937e8ea2f053f15c3ac911c0b6e
Christopher Hauschild Schott <chrishaus01@gmail.com>
2020-04-10 18:43:10 -0300

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,55 @@
+var acaoEscolhida = "";
+var valorTela = "";
+var valorAntigoTela = "";
+
+function botaoDigitoClick(event) {
+    var digito = event.target.value;
+    atualizaTela(`${valorTela}${digito}`);
+}
+
+function botaoAcaoClick(event) {
+    var acao = event.target.value;
+    rodarAcao(acao);
+}
+
+function atualizaTela(valor) {
+    var tela = document.getElementById("tela");
+    valorTela = valor;
+    tela.innerHTML = valorTela;
+}
+
+function rodarAcao(acao) {
+    if (acao === "=") {
+        var resultado;
+
+        if (acaoEscolhida === "+") {
+            resultado = parseInt(valorAntigoTela) + parseInt(valorTela);
+        }
+
+        if (acaoEscolhida === "-") {
+            resultado = parseInt(valorAntigoTela) - parseInt(valorTela);
+        }
+
+        if (acaoEscolhida === "*") {
+            resultado = parseInt(valorAntigoTela) * parseInt(valorTela);
+        }
+
+        if (acaoEscolhida === "/") {
+            resultado = parseInt(valorAntigoTela) / parseInt(valorTela);
+        }
+
+        atualizaTela(resultado);
+
+    } else {
+
+        if (acao === "c") {
+            location.reload();
+        }
+
+        valorAntigoTela = valorTela;
+        atualizaTela("");
+        acaoEscolhida = acao;
+    }
+}
+
+
